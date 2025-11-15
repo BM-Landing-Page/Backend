05d76f35d25ca227cc985a95fb7c0237f785efde
gopal-vsg <saigopalvallu7@gmail.com>
2025-11-15 06:43:21 +0530

    changes

--- routes/alumniRoutes.js
+++ routes/alumniRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getAllAlumni, getAlumnusById, createAlumnus, updateAlumnus, deleteAlumnus } from '../controllers/alumniController.js';
-import { authMiddleware } from '../middleware/authMiddleware.js';
+import { authenticateToken } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
@@ -8,9 +8,9 @@ const router = express.Router();
 router.get('/', getAllAlumni);
 router.get('/:id', getAlumnusById);
 
-// Protected
-router.post('/', authMiddleware, createAlumnus);
-router.put('/:id', authMiddleware, updateAlumnus);
-router.delete('/:id', authMiddleware, deleteAlumnus);
+	// Protected
+	router.post('/', authenticateToken, createAlumnus);
+	router.put('/:id', authenticateToken, updateAlumnus);
+	router.delete('/:id', authenticateToken, deleteAlumnus);
 
 export default router;
